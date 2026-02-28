3ebd627a917fa76027669b429133c5a036ba8348
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-02-28 22:08:41 +0200

    target SDK 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "com.nicos.eye_dropper_api"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = preview("CinnamonBun")
     }
 
     defaultConfig {
@@ -31,12 +29,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
     }
+    buildToolsVersion = "37.0.0 rc2"
 }
 
 dependencies {
